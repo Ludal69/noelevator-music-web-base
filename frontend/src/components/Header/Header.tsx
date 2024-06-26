073f474fe29ea2e04722ecd32f541e971f28aa02
DESKTOP-G4I7JNL\Ludo <lulu.713lg@gmail.com>
2024-06-26 11:55:03 +0200

    - links header - modifier taille integration youtube

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,29 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
-import logo from "../../assets/Images/no_elevator_gf_logo.svg"; // Import correct de l'image
+import logo from "../../assets/images/no_elevator_gf_logo.svg";
 
 const Header: React.FC = () => {
   return (
     <header className="w-full bg-custom-blue text-custom-yellow fixed top-0 left-0 z-50 h-20 lg:h-24">
       <div className="container mx-auto flex justify-between items-center px-4 h-full">
         <nav className="flex space-x-4 text-lg w-2/5 justify-center items-center h-full font-titles">
-          <Link to="/" className="hover:underline">
+          <Link to="/listen" className="hover:underline">
+            Listen
+          </Link>
+          <Link to="/shows" className="hover:underline">
+            Shows
+          </Link>
+          <Link to="/store" className="hover:underline">
             Store
           </Link>
-          <Link to="/live" className="hover:underline">
-            Live
+          <Link to="/bio" className="hover:underline">
+            Bio
           </Link>
-          <Link to="/videos" className="hover:underline">
-            Videos
+          <div className="relative group">
+            <button className="hover:underline">Media</button>
+            <div className="absolute hidden group-hover:block bg-gray-700 text-white rounded">
+              <Link
+                to="/photos"
+                className="block px-4 py-2 hover:bg-gray-600 bg-custom-blue text-custom-yellow"
+              >
+                Photos
+              </Link>
+              <Link
+                to="/videos"
+                className="block px-4 py-2 hover:bg-gray-600 bg-custom-blue text-custom-yellow"
+              >
+                Videos
+              </Link>
+            </div>
+          </div>
+          <Link to="/contact" className="hover:underline">
+            Contact
           </Link>
         </nav>
-        <div className="w-1/5 flex justify-center items-center h-full">
-          <img
-            src={logo}
-            alt="Group Logo"
-            className="inline-block h-16 lg:h-20 py-2" // Utilisation des classes Tailwind pour hauteur responsive
-          />
+        <div className="w-1/5 flex justify-center items-center h-full font-logo">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Group Logo"
+              className="inline-block h-16 lg:h-20 py-2"
+            />
+          </Link>
         </div>
         <div className="flex space-x-4 w-2/5 justify-center items-center h-full">
           <SocialIcon
@@ -39,13 +64,13 @@ const Header: React.FC = () => {
             bgColor="transparent"
           />
           <SocialIcon
-            url="https://youtube.com"
+            url="https://twitch.tv"
             style={{ height: 50, width: 50 }}
             fgColor="#ffac49"
             bgColor="transparent"
           />
           <SocialIcon
-            url="https://spotify.com"
+            url="https://tiktok.com"
             style={{ height: 50, width: 50 }}
             fgColor="#ffac49"
             bgColor="transparent"
--- frontend/src/components/YoutubeVideo/YoutubeVideo.tsx
+++ frontend/src/components/YoutubeVideo/YoutubeVideo.tsx
@@ -7,9 +7,9 @@ interface YouTubeVideoProps {
 const YouTubeVideo: React.FC<YouTubeVideoProps> = ({ videoUrl }) => {
   return (
     <div className="w-full flex justify-center my-8">
-      <div className="w-11/12 md:w-9/12 lg:w-8/12">
+      <div className="w-9/12 md:w-8/12 lg:w-8/12">
         <iframe
-          className="w-full h-64 md:h-96"
+          className="w-full h-96 md:h-128"
           src={videoUrl}
           title="YouTube video player"
           frameBorder="0"
