9ab9e1c49f86f52f1f5eba6d0ba4d6f6b1515f61
DESKTOP-G4I7JNL\Ludo <lulu.713lg@gmail.com>
2024-07-17 12:40:35 +0200

    front on store page. Same size for all images

--- frontend/src/components/ProductCard/ProductCard.tsx
+++ frontend/src/components/ProductCard/ProductCard.tsx
@@ -8,8 +8,10 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ image, title, price }) => {
   return (
-    <div className="border-2 border-custom-yellow rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300">
-      <img src={image} alt={title} className="w-full h-64 object-cover" />
+    <div className="rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 w-64">
+      <div className="w-full h-64 overflow-hidden flex justify-center items-center">
+        <img src={image} alt={title} className="object-contain w-full h-full" />
+      </div>
       <div className="p-4">
         <h2 className="font-bold text-lg">{title}</h2>
         <p className="text-custom-yellow text-xl">{price}</p>
